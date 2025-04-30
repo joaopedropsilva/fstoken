596eafa5614aa935683aac619dbaaf28b15397e4
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2025-04-30 19:20:30 -0300

    removed standard cli options

--- src/cli.py
+++ src/cli.py
@@ -1,43 +1,5 @@
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
-from src.nacl import Crypto, Token
-
-
-def sign_tokenize() -> None:
-    token = Token(bytes(), b"Attack")
-
-
-def call_encrypt_routine(parsed_args: Namespace) -> None:
-    file = parsed_args.file
-    if file is None:
-        print("Please, specify --file or -f with the desired file path for " \
-              "encryption.")
-        exit(1)
-    file = Path(file)
-    if not file.exists():
-        print(f"File {str(file)} not found!")
-        exit(1)
-
-    Crypto.encrypt(file)
-
-
-def call_decrpyt_routine(parsed_args: Namespace) -> None:
-    file = parsed_args.file
-    if file is None:
-        print("Please, specify --file or -f with the desired file path for " \
-              "decryption.")
-        exit(1)
-    file = Path(file)
-    if not file.exists():
-        print(f"File {str(file)} not found!")
-        exit(1)
-    key = parsed_args.key
-    if key is None:
-        print("Please, specify --key or -k with the key for decryption.")
-        exit(1)
-    key = key.strip()
-
-    Crypto.decrypt(file, key)
 
 
 if __name__ == "__main__":
