5c8784aaa109bacb51157c363c4154b6bceb02b5
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2025-05-24 12:36:26 -0300

    make cli flows more readable

--- src/cli.py
+++ src/cli.py
@@ -7,33 +7,60 @@ from src.file import File
 from src.helpers import log
 
 
+def _handle_deletion(args: Namespace,
+                     was_encrypted: bool,
+                     prev_key: str) -> None:
+    if prev_key == "":
+        log(f"File not found in {Keystore.STORE_FILENAME}", args.verbose)
+        exit(1)
+
+    log(f"Removing from {Keystore.STORE_FILENAME}", args.verbose)
+    Keystore.change_entry(args.file, delete=True)
+
+    if was_encrypted:
+        if not File.decrypt(args.file, prev_key):
+            exit(1)
+
+    exit(0)
+
+
+def _handle_invocation(args: Namespace) -> None:
+    pass
+
+
 def handle_call(args: Namespace) -> None:
     Fskeys.init(verbose=args.verbose)
 
     (was_encrypted, prev_key) = Keystore.search_entry_state(args.file)
-    if args.delete and prev_key == "":
-        log_if_verbose("File not found in keystore", args.verbose)
-        return
+
+    is_deletion = args.delete
+    is_delegation = args.token and args.grant and args.subject
+    is_invocation = args.token and not is_delegation
+
+    if is_deletion:
+        _handle_deletion(args, was_encrypted, prev_key)
+    if is_invocation:
+        _handle_invocation(args)
 
     filekey = Keystore.change_entry(args.file,
-                                    args.encrypt,
-                                    args.rotate,
-                                    args.delete)
+                                    encrypt=args.encrypt,
+                                    rotate_key=args.rotate,
+                                    delete=False)
 
     if was_encrypted:
-        File.decrypt(args.file, prev_key)  # Prevents content lost if key rotates
+        if not File.decrypt(args.file, prev_key):
+            exit(1)
     if args.encrypt:
-        File.encrypt(args.file, filekey)
+        if not File.encrypt(args.file, filekey):
+            exit(1)
 
-    if args.grant == "" or args.subject == "":
-        return
-
-    (private, _) = Fskeys.get_keys()
-    token = Token.encode(private, raw_payload={"file_key": filekey,
-                                               "grant": args.grant,
-                                               "subject": args.subject,
-                                               "proof": [args.token]})
-    print(token)
+    if is_delegation:
+        (private, _) = Fskeys.get_keys()
+        token = Token.encode(private, raw_payload={"file_key": filekey,
+                                                   "grant": args.grant,
+                                                   "subject": args.subject,
+                                                   "proof": [args.token]})
+        print(token)
 
 
 if __name__ == "__main__":
@@ -42,6 +69,7 @@ if __name__ == "__main__":
                             "file access control using a semi capabilities " \
                             "model and encryption.")
     parser.add_argument("file")
+    parser.add_argument("--verbose", "-v", action="store_true")
     parser.add_argument("--encrypt", "-e", action="store_true")
     parser.add_argument("--rotate", "-r", action="store_true")
     parser.add_argument("--delete", "-d", action="store_true")
--- src/file.py
+++ src/file.py
@@ -6,6 +6,10 @@ from src.helpers import log
 
 
 class File:
+    _OP_NOT_PERMITTED_MSG = \
+        "Unable to {op} file: {file} (operation not permitted)\n" \
+        "Make sure you have the correct access rights to perform this operation"
+
     @staticmethod
     def _rewrite_file(filepath: Path, content_producer_fn: callable) -> None:
         with open(filepath, "r+b") as file:
@@ -26,26 +30,9 @@ class File:
                 return NaclBinder.secretbox_decrypt(encrypted, b64key) \
                         .decode("utf-8")
         except PermissionError:
-            log_if_verbose(
-                f"Unable to decrypt: {args.file}, operation not permitted",
-                verbose=True
-            )
+            log(cls._OP_NOT_PERMITTED_MSG.format(op="decrypt", file=file),
+                verbose=True)
             return ""
-
-    @classmethod
-    def encrypt(cls, file: str, b64key: bytes | str) -> bool:
-        filepath = Path(file)
-        encrypt_fn = partial(NaclBinder.secretbox_encrypt, b64key)
-        try:
-            cls._rewrite_file(filepath, encrypt_fn)
-        except PermissionError:
-            log_if_verbose(
-                f"Unable to encrypt: {args.file}, operation not permitted",
-                verbose=True
-            )
-            return False
-
-        return True
 
     @classmethod
     def decrypt(cls, file: str, b64key: bytes | str) -> bool:
@@ -54,10 +41,21 @@ class File:
         try:
             cls._rewrite_file(filepath, decrypt_fn)
         except PermissionError:
-            log_if_verbose(
-                f"Unable to decrypt: {args.file}, operation not permitted",
-                verbose=True
-            )
+            log(cls._OP_NOT_PERMITTED_MSG.format(op="decrypt", file=file),
+                verbose=True)
+            return False
+
+        return True
+
+    @classmethod
+    def encrypt(cls, file: str, b64key: bytes | str) -> bool:
+        filepath = Path(file)
+        encrypt_fn = partial(NaclBinder.secretbox_encrypt, b64key)
+        try:
+            cls._rewrite_file(filepath, encrypt_fn)
+        except PermissionError:
+            log(cls._OP_NOT_PERMITTED_MSG.format(op="encrypt", file=file),
+                verbose=True)
             return False
 
         return True
