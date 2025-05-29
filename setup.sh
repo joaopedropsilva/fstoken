6a794b94bd414e6a1ca95d1ed8d22e0f11db0cf6
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2025-05-29 07:30:33 -0300

    refining setup.sh
    still need to fix permissions

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 CWD="$(realpath "$(dirname @)")"
 DAEMON=fstokend.service
@@ -9,6 +9,24 @@ if systemctl is-active --quiet $DAEMON; then
     sudo systemctl stop $DAEMON
 fi
 
+echo "Installing fstoken at $APP_PREFIX"
+sudo rm -rf $APP_PREFIX
+sudo mkdir $APP_PREFIX
+sudo cp $CWD/requirements.txt $APP_PREFIX/requirements.txt
+sudo cp $CWD/setup.sh $APP_PREFIX/setup.sh
+sudo cp $CWD/fstn $APP_PREFIX/fstn
+sudo cp $CWD/$DAEMON $APP_PREFIX/$DAEMON
+sudo cp -r $CWD/src $APP_PREFIX/src
+
+echo "Setting up permissions"
+sudo chown --recursive fstoken:fstoken $APP_PREFIX
+sudo chmod --recursive 755 $APP_PREFIX
+sudo chmod 440 $APP_PREFIX/$DAEMON
+
+echo "Creating python virtual environment"
+sudo /usr/bin/python3 -m venv $APP_PREFIX/venv
+sudo $APP_PREFIX/venv/bin/python -m pip install -r requirements.txt
+
 echo "Creating fsktoken user"
 sudo useradd \
     --system \
@@ -18,24 +36,11 @@ sudo useradd \
     fstoken
 sudo usermod -L fstoken
 
-echo "Installing fstoken at /opt"
-sudo rm -rf $APP_PREFIX
-sudo mkdir $APP_PREFIX
-sudo cp $CWD/requirements.txt $APP_PREFIX/requirements.txt
-sudo cp $CWD/$DAEMON $APP_PREFIX/$DAEMON
-sudo cp -r $CWD/src $APP_PREFIX/src
-
-echo "Creating python virtual environment"
-sudo /usr/bin/python3 -m venv $APP_PREFIX/venv
-sudo $APP_PREFIX/venv/bin/python -m pip install -r requirements.txt
-
-echo "Setting up permissions"
-sudo chown --recursive fstoken:fstoken $APP_PREFIX
-sudo chmod --recursive 550 $APP_PREFIX
-sudo chmod 440 $APP_PREFIX/$DAEMON
-
 echo "Setting up daemon"
 sudo cp $APP_PREFIX/$DAEMON /etc/systemd/system
 sudo systemctl daemon-reexec
 sudo systemctl enable --now $DAEMON
+
+echo "Adding cli to user PATH"
+export PATH=$PATH:$APP_PREFIX
 
